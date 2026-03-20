8ef59ed8e4917e08d2f60d5ed232c399219a6cd1
AkashMondal27 <akashmondal102003@gmail.com>
2026-03-20 11:39:48 +0530

    first commit

---
+++ Basic/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config() 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+// another response
+app.get('/twitter' ,(req ,res)=>{
+    res.send("akashX")
+})
+
+app.get( '/login' , ( req, res)=>{
+    res.send("<h1> login here .....</h1>")
+})
+
+
+app.get('/Youtube' , ( req,res)=>{
+        res.send(" blue sky youtube ")
+})
+app.listen( process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
