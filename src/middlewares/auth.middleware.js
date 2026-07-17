e07d4b48ef17a2727e9028dfe626b4eb706ccf7f
AkashMondal27 <akashmondal102003@gmail.com>
2026-07-17 09:31:21 +0530

    fix bugs on auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,7 +14,7 @@ export const verifyJWT= asyncHandler(async(req,res,next)=>{
             throw new ApiError(401 , "Unauthorized request")
          }
     
-         const decodedToken= await jtw.verify(token, process.env.ACCESS_TOKEN_SECRET)
+         const decodedToken= await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
          const user=  await  User.findById(decodedToken?._id).select("-password -refreshToken")
     
